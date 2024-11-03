cb76d4404ff9279fae60fc4e3536010761da067e
MaksymMalicki <maksym.malicki.2115@gmail.com>
2024-11-03 23:03:41 +0100

    add radars

--- src.py
+++ src.py
@@ -8,7 +8,8 @@ WHITE = (255, 255, 255)
 ROAD_COLOR = (50, 50, 50)
 ROAD_LEFT = 250
 ROAD_RIGHT = 550
-CAR_IMG = pygame.transform.scale(pygame.image.load("car.png"), (100, int(100 * pygame.image.load("car.png").get_height() / pygame.image.load("car.png").get_width())))
+CAR_WIDTH = 50
+CAR_IMG = pygame.transform.scale(pygame.image.load("car.png"), (CAR_WIDTH, int(CAR_WIDTH * pygame.image.load("car.png").get_height() / pygame.image.load("car.png").get_width())))
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("NEAT Car Simulation")
 
@@ -18,6 +19,8 @@ class Car:
         self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 50))
         self.speed = 0
         self.rotation = 0
+        self.radars_count = 6
+        self.radars = [0] * self.radars_count
 
     def move(self):
         radians = math.radians(self.rotation)
@@ -33,6 +36,22 @@ class Car:
         if self.rect.left < ROAD_LEFT or self.rect.right > ROAD_RIGHT:
             return True
         return False
+    
+    def radar(self, angle):
+        x_center, y_center = self.rect.center
+        length = 0
+        x1, y1 = x_center + length * math.sin(angle), y_center - length * math.cos(angle)
+        while length < 500 and ROAD_LEFT < x1 < ROAD_RIGHT and 0 < y1 < HEIGHT:
+            length += 1
+            x1, y1 = x_center + length * math.sin(angle), y_center - length * math.cos(angle)
+        pygame.draw.line(win, (0, 255, 0), (x_center, y_center), (x1, y1))
+        return math.hypot(x1 - x_center, y1 - y_center)
+        
+    def check_radars(self):
+        for i in range(self.radars_count):
+            angle = math.radians(self.rotation - 90 + i * (180 // (self.radars_count - 1)))
+            self.radars[i] = self.radar(angle)
+        print(self.radars)
 
 def run():
     car = Car()
@@ -63,7 +82,7 @@ def run():
             car.rotation += 2
 
         car.move()
-
+        car.check_radars()
         if car.check_collision():
             print("Collision with road boundary!")
 
